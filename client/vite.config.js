75e5886bc1be5b42944962e5cbca0bd457d63275
maneesha-9705 <maneeshag099@gmail.com>
2026-05-18 16:50:39 +0530

    Configure Vite chunking to resolve Vercel bundle size warnings

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1500, // Increase warning limit to 1.5MB
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Split all external libraries into a vendor chunk
+          }
+        }
+      }
+    }
+  }
 })
